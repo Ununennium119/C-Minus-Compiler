319fd89adbdb6fe046dcfe8b90784b30c5eaf8fd
Mohammad Sadeghi <moh02sadeghi@yahoo.com>
2022-11-26 17:19:17 +0330

    refactoring

--- scanner.py
+++ scanner.py
@@ -69,6 +69,7 @@ class Error(Enum):
 class Scanner:
     BUFFER_SIZE = 1024
 
+    # character sets
     EOF = None
     all_chars: Set[str] = set(chr(i) for i in range(128))
     digits: Set[str] = set(string.digits)
@@ -78,106 +79,121 @@ class Scanner:
     whitespaces: Set[str] = {' ', '\n', '\r', '\t', '\v', '\f'}
     valid_chars: Set[str] = alphanumerics.union(symbols, whitespaces)
 
+    # token types
+    NUM: str = "NUM"
+    ID: str = "ID"
+    KEYWORD: str = "KEYWORD"
+    SYMBOL: str = "SYMBOL"
+    COMMENT: str = "COMMENT"
+    WHITESPACE: str = "WHITESPACE"
+
     keywords = {"if", "else", "void", "int", "while", "break", "switch", "default", "case", "return", "endif"}
 
     def __init__(self):
         """Inits Scanner"""
-        """ Input storage"""
+        # Input storage
         self.input_file = open("input.txt", mode="r")
         self.buffer: List[Optional[str]] = []
 
-        """ Error storage"""
+        # Error storage
         self.error_file = open("lexical_errors.txt", mode="w")
-        self.error_count = 0
+        self.error_count: int = 0
 
-        """ Symbol Table """
-        self.symbol_table: Dict[str, List[Optional]] = {}
+        # Symbol Table
         self._initialize_symbol_table()
         self.symbol_table_file = open("symbol_table.txt", mode="w")
 
-        """ Position in buffer """
+        # Position in buffer
         self.forward: int = 0
-        self.current_char: str = None
+        self.current_char: Optional[str] = None
+        self.line_number: int = 0
 
-        """ Token temporary storage """
-        self.token_buffer: str = ""
+        # Token temporary storage
+        self.token_buffer: List[str] = []
 
-        """ State"""
+        # States
         self._initialize_states()
         self.current_state: State = self.states[0]
+
+    @property
+    def current_token(self) -> Optional[str]:
+        if self.token_buffer is not None:
+            return ''.join(self.token_buffer)
+        else:
+            return None
 
     def handle_error(self, error: Error):
         # TODO: Also implement Panic Mode
         self.error_count += 1
         if error == Error.NO_TRANSITION:
-            if self.current_state == self.states[1] and self.current_char in self.alphanumerics:
-                error_tuple = (self.token_buffer, "Invalid number")
-            elif self.current_state == self.states[17] and self.current_char == '/':
-                error_tuple = (self.token_buffer, "Unmatched comment")
+            if self.current_state.number == 1 and self.current_char in self.alphanumerics:
+                error_tuple = (self.current_token, "Invalid number")
+            elif self.current_state.number == 17 and self.current_char == '/':
+                error_tuple = (self.current_token, "Unmatched comment")
             else:
-                error_tuple = (self.token_buffer, "Invalid input")
+                error_tuple = (self.current_token, "Invalid input")
         elif error == Error.INCOMPLETE_TOKEN:
-            if self.current_state == self.states[13] or \
-                    self.current_state == self.states[14]:
-                error_tuple = (self.token_buffer[:min(9, len(self.token_buffer))] + "...", "Unclosed comment")
+            if self.current_state.number in {13, 14}:
+                error_tuple = (f"{''.join(self.token_buffer[:9])} ...", "Unclosed comment")
+            else:
+                error_tuple = (self.current_token, "Undefined Error!")
         else:
-            error_tuple = (self.token_buffer, "Undefined Error!")
-        self.error_file.write(str(error_tuple) + "\n")
+            error_tuple = (self.current_token, "Undefined Error!")
+        self.error_file.write(f"{error_tuple}\n")
 
-    def token_tuple(self):
-        if self.current_state == self.states[2]:
-            return "NUM", self.token_buffer
-        elif self.current_state == self.states[4]:
+    def get_token_tuple(self) -> Tuple[str, str]:
+        if self.current_state.number == 2:
+            return self.NUM, self.current_token
+        elif self.current_state.number == 4:
             token_type = self.get_token_type()
-            return token_type, self.install_id(token_type)
-        elif self.current_state == self.states[5] or \
-                self.current_state == self.states[7] or \
-                self.current_state == self.states[8] or \
-                self.current_state == self.states[10] or \
-                self.current_state == self.states[18]:
-            return "SYMBOL", self.token_buffer
-        elif self.current_state == self.states[12] or \
-                self.current_state == self.states[15]:
-            return "COMMENT", self.token_buffer
-        elif self.current_state == self.states[16]:
-            return "WHITESPACE", self.token_buffer
+            return token_type, self.install_id()
+        elif self.current_state.number in {5, 7, 8, 10, 18}:
+            return self.SYMBOL, self.current_token
+        elif self.current_state.number in {12, 15}:
+            return self.COMMENT, self.current_token
+        elif self.current_state.number == 16:
+            return self.WHITESPACE, self.current_token
 
-    def get_token_type(self):
-        if self.token_buffer in Scanner.keywords:
-            return "KEYWORD"
-        return "ID"
+    def get_token_type(self) -> str:
+        """Return \"KEYWORD\" if current token is a keyword else \"ID\"."""
+        if self.current_token in self.keywords:
+            return self.KEYWORD
+        return self.ID
 
-    def install_id(self, token_type):
-        if token_type == "ID":
-            if not self.token_buffer in self.symbol_table:
-                self.symbol_table[self.token_buffer] = [len(self.symbol_table) + 1]
-        return self.token_buffer
+    def install_id(self):
+        """Adds current id to symbol table if it is not."""
+        token: str = self.current_token
+        if token not in self.symbol_table:
+            self.symbol_table[token] = [len(self.symbol_table) + 1]
+        return token
 
-    def get_next_token(self) -> Tuple:
+    def get_next_token(self) -> Optional[Tuple]:
         """Return next token of input_file. None if EOF."""
-        self.token_buffer = ""
+        self.token_buffer.clear()
         self.current_state = self.states[0]
         while True:
-            """ Terminal state"""
+            # Check terminal state
             if self.current_state.is_terminal:
+                # Terminal state
                 if self.current_state.is_lookahead:
-                    self.token_buffer = self.token_buffer[:-1]
+                    self.token_buffer.pop()
                     self.forward_step_back()
-                return self.token_tuple()
+                return self.get_token_tuple()
 
-            """ Non-terminal state """
+            # Non-terminal state
             self.current_char = self.get_next_char()
-            # EOF
+            # Check EOF
             if self.current_char is None:
+                # EOF
                 if self.current_state.number in {1, 3, 6, 9, 11}:
                     self.forward_step_back()
-                    return self.token_tuple()
+                    return self.get_token_tuple()
                 self.handle_error(Error.INCOMPLETE_TOKEN)
                 return None
 
             # not EOF
-            self.token_buffer += self.current_char
-            """ Choosing appropriate transition """
+            self.token_buffer.append(self.current_char)
+            # Choosing appropriate transition
             for transition in self.current_state.out_transitions:
                 if self.current_char in transition.charset:
                     self.current_state = transition.dest
@@ -186,7 +202,7 @@ class Scanner:
                 self.handle_error(Error.NO_TRANSITION)
 
     def forward_step_back(self):
-        """ Move <forward> backward """
+        """Move <forward> back"""
         if self.forward == 0:
             self.forward = 2 * self.BUFFER_SIZE - 1
         else:
@@ -217,6 +233,7 @@ class Scanner:
         return [temp[i] if i < len(temp) else None for i in range(self.BUFFER_SIZE)]
 
     def _initialize_symbol_table(self):
+        self.symbol_table: Dict[str, List[Optional]] = {}
         for keyword in Scanner.keywords:
             self.symbol_table[keyword] = [len(self.symbol_table) + 1]
 
@@ -242,12 +259,12 @@ class Scanner:
         self.states[0].add_transition(Transition(self.states[0], self.states[16], self.whitespaces))
         self.states[0].add_transition(Transition(self.states[0], self.states[17], {'*'}))
         self.states[1].add_transition(Transition(self.states[1], self.states[1], self.digits))
-        self.states[1].add_transition(Transition(self.states[1], self.states[2], self.symbols.union(self.whitespaces)))
+        self.states[1].add_transition(Transition(self.states[1], self.states[2], self.symbols.union(self.whitespaces, self.EOF)))
         self.states[3].add_transition(Transition(self.states[3], self.states[3], self.alphanumerics))
-        self.states[3].add_transition(Transition(self.states[3], self.states[4], self.symbols.union(self.whitespaces)))
+        self.states[3].add_transition(Transition(self.states[3], self.states[4], self.symbols.union(self.whitespaces, self.EOF)))
         self.states[6].add_transition(Transition(self.states[6], self.states[7], {'='}))
-        self.states[6].add_transition(Transition(self.states[6], self.states[8], self.valid_chars - {'='}))
-        self.states[9].add_transition(Transition(self.states[9], self.states[10], self.valid_chars - {'*', '/'}))
+        self.states[6].add_transition(Transition(self.states[6], self.states[8], self.valid_chars.union(self.EOF) - {'='}))
+        self.states[9].add_transition(Transition(self.states[9], self.states[10], self.valid_chars.union(self.EOF) - {'*', '/'}))
         self.states[9].add_transition(Transition(self.states[9], self.states[11], {'/'}))
         self.states[9].add_transition(Transition(self.states[9], self.states[13], {'*'}))
         self.states[11].add_transition(Transition(self.states[11], self.states[11], self.all_chars - {'\n'}))
@@ -269,5 +286,5 @@ if __name__ == '__main__':
         print("current token: ", current_token)
     if scanner.error_count == 0:
         scanner.error_file.write("There is no lexical error.")
-    for key in scanner.symbol_table:
-        scanner.symbol_table_file.write(str(scanner.symbol_table[key]) + ": " + key + "\n")
+    for key, value in scanner.symbol_table.items():
+        scanner.symbol_table_file.write(f"{value[0]}.	{key}\n")
